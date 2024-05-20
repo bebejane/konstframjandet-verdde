fa6f0b1fe7110e1898beed72f31305ce0a5b5e1d
Bebe Jane <bjornthief@gmail.com>
2024-05-20 13:50:17 +0200

    aah

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "./globals.scss";
+import "./style.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        Verdde
-      </main>
+      <div className="wrapper">
+        <figure>
+          <img src="https://cdn.glitch.global/7f2ed5cd-b85b-4c16-9571-1ad36768dace/verdde.png?v=1715087697425" />
+        </figure>
+        <section className="text">
+          <span className="intro">Buoris, vad roligt att du hittat hit!<br /></span>
+          Kom tillbaka igen under höstsommaren för mer innehåll.<br />
+          Tills dess finns vi på <a href="https://www.instagram.com/verddekonst">@verddekonst</a> och <a href="mailto:info@verdde.net">info@verdde.net</a></section>
+      </div>
     </>
   );
 }

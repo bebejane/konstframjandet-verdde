9d36ef706cb0b8179ad0488c362721ecd4d5f43f
Bebe Jane <bjornthief@gmail.com>
2024-08-22 15:33:50 +0200

    ok

--- pages/api/web-previews.ts
+++ pages/api/web-previews.ts
@@ -1,11 +1,11 @@
-import { withWebPreviews } from 'dato-nextjs-utils/hoc';
-import { recordToSlug } from '/lib/routes';
+import { withWebPreviewsEdge } from 'dato-nextjs-utils/hoc';
+import { recordToSlug } from '../../lib/routes';
 
 export const config = {
-  runtime: 'nodejs'
+  runtime: 'edge'
 }
 
-export default withWebPreviews(async ({ item, itemType }) => {
+export default withWebPreviewsEdge(async ({ item, itemType }) => {
   const path = recordToSlug(item.attributes, itemType.attributes.api_key)
   return path
 })

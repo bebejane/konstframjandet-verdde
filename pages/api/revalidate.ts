4d3feb78eb1dbdd27f399755d4c986b0942e2a67
Bebe Jane <bjornthief@gmail.com>
2024-08-22 15:22:02 +0200

    fixat

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,4 +1,5 @@
 import { withRevalidate } from 'dato-nextjs-utils/hoc'
+import path from 'path';
 import { categories } from '/lib/constant';
 
 export default withRevalidate(async (record, revalidate) => {
@@ -12,14 +13,17 @@ export default withRevalidate(async (record, revalidate) => {
       paths.push(`/`)
       break;
     case 'about':
+      paths.push('/')
       paths.push(`/om/${slug}`)
       break;
     case 'program': case 'participant': case 'partner':
       const category = categories.find(c => c._apiKey === api_key)
+      paths.push('/')
       paths.push('/vad-vi-gor')
       category && paths.push(`/vad-vi-gor/${category.slug}/${slug}`)
       break;
     case 'news':
+      paths.push('/')
       paths.push('/pa-gang')
       paths.push(`/pa-gang/${slug}`)
       break;

f0979f9fb7ff855d1b3ef8e500d7d1463aef5a08
konst-teknik <information@konst-teknik.se>
2024-09-06 16:54:00 +0200

    Fix

--- pages/pa-gang/index.tsx
+++ pages/pa-gang/index.tsx
@@ -19,7 +19,7 @@ export default function News({ news, general }: Props) {
   return (
     <>
       <DatoSEO title={'Vad vi gör'} />
-      <PageHeader header={general.aboutSv} headerSmi={general.aboutSmi} content={general.aboutIntro} />
+      <PageHeader header={general.newsSv} headerSmi={general.newsSmi} content={general.newsIntro} />
       <CardContainer key={asPath}>
         {news.map(({ id, title, _publishedAt, image, slug, intro }) =>
           <Card key={id}>
